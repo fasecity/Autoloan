1bea556ea0716eb47b79b80b4ebdeb5ac443f637
admin <admin@DESKTOP-NNLVJHQ>
2017-03-30 16:33:48 -0400

    Add project files.

---
+++ Autoloan/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Autoloan.Startup))]
+namespace Autoloan
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
