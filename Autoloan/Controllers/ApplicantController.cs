74950e7b3ee76f47e709fb47f60d058da258f320
audiotrash112@gmail.com <audiotrash112@gmail.com>
2017-03-31 02:42:28 -0400

    needs a ton of work phone needs to be double

---
+++ Autoloan/Controllers/ApplicantController.cs
@@ -0,0 +1,60 @@
+﻿using Autoloan.Models;
+using Autoloan.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Autoloan.Controllers
+{
+    public class ApplicantController : Controller
+    {
+        private ApplicationDbContext _context;
+        public ApplicantController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        // GET: Applicant
+        public ActionResult Create()
+        {
+            var viewModels = new ApplicantFormViewModel
+            {
+
+                //car insurances comes from view model
+                CarInsurances = _context.CarInsurancs.ToList()
+
+            };
+
+            return View(viewModels);
+
+        }
+        [HttpPost]
+        public ActionResult Create(ApplicantFormViewModel viewModel)
+        {
+            var carInsu = _context.CarInsurancs.Single(m => m.Id == viewModel.CarInsurance);
+            var applicant = new Applicant
+            {
+                FirstName = viewModel.FirstName,
+                LastName = viewModel.LastName,
+                Age = viewModel.Age,
+                PhoneNumber = viewModel.PhoneNumber,
+                CarInsurance = carInsu,
+                Carkilometer = viewModel.Carkilometer,
+                DesiredLoanAmmount = viewModel.DesiredLoanAmmount,
+                DateTime = DateTime.Now,
+                MonthlyIncome = viewModel.MonthlyIncome,
+                CarMake = viewModel.CarMake,
+                CarModel = viewModel.CarModel,
+                CarYear = viewModel.CarYear,
+                Email = viewModel.Email,
+                Province = viewModel.Province
+
+            };
+            _context.Applicants.Add(applicant);
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Home");
+            //return View();
+        }
+    }
+}
---
+++ Autoloan/ViewModels/ApplicantFormViewModel.cs
@@ -0,0 +1,68 @@
+﻿using Autoloan.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Autoloan.ViewModels
+{
+    public class ApplicantFormViewModel
+    {
+       // public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string LastName { get; set; }
+
+        [Required]
+        //[MaxLength(99)]
+        //[MinLength(18)]
+        public int Age { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Province { get; set; }
+
+        [Required]
+        //[MaxLength(10)]
+        public int PhoneNumber { get; set; }
+
+        [Required]
+        //[EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string CarMake { get; set; }
+
+        [Required]
+        public int CarYear { get; set; }
+
+        [Required]
+        //[MaxLength(250000)]
+        public int Carkilometer { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string CarModel { get; set; }
+
+        [Required]
+        public double DesiredLoanAmmount { get; set; }
+
+        [Required]
+        public double MonthlyIncome { get; set; }
+
+
+        public DateTime DateTime { get; set; }
+
+        [Required]
+        //genreId as stored in Db
+        public int CarInsurance { get; set; }
+        public IEnumerable<CarInsurance> CarInsurances { get; set; }
+    }
+}
